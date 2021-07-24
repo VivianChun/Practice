33cd140474c99703d69e07a4920b92f41270b581
Vivian Weng <f0939618858@gmail.com>
2021-07-24 16:48:15 +0800

    first commit

---
+++ whilemode.py
@@ -0,0 +1,10 @@
+while True:
+    mode= input('please input mode:')
+    if mode == 'q':
+    	break
+    elif mode == '1':
+    	print ('start mode 1')
+    elif mode == '2':
+    	print ('start mode 2')
+    else:
+    	print ('Only input 1 or 2 or q')
